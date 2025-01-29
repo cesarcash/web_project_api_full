5a799835b864a71a488f1a2ba393342981cd4792
cesarcash <cesarcash5@gmail.com>
2025-01-29 16:05:12 -0600

    constants

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = `api.cesarcash.chickenkiller.com`;
+export const API_URL = `https://api.cesarcash.chickenkiller.com`;
 
 export const ApiConfigHeaders = {
   'Content-Type': 'application/json'
